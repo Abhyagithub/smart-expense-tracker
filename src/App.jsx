24464f8097628764f511cf0be73528a9f72ee9f3
Abhyagithub <abhishekamrad@gmail.com>
2025-07-31 21:13:52 +0530

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-4xl font-bold text-blue-500">
+        Smart Expense Tracker 💰
+      </div>
+    </div>
+  );
+}
+export default App;
+
